81004b79ff0f90fa3ebfb7cbc7095d2a9f1ba9c1
Jeongminseok-web <jeong010328@gmail.com>
2024-09-12 15:05:55 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Area from "./components/Area";
+import Theme from "./components/Theme";
+import Hot from "./components/Hot";
+import Visited from "./components/Visited";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/area" element={<Area />} />
+          <Route path="/theme" element={<Theme />} />
+          <Route path="/hot" element={<Hot />} />
+          <Route path="/visited" element={<Visited />} />
+        </Routes>
+
+        <ToastContainer
+          position="top-center"
+          autoClose={1000}
+          theme="light"
+          closeOnClick
+        />
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/utils/apiUrl.js
@@ -0,0 +1,22 @@
+const GET_AREAS_API_URL = `${process.env.REACT_APP_MY_DOMAIN}/get_areas`;
+const POST_AREAS_API_URL = `${process.env.REACT_APP_MY_DOMAIN}/get_areas`;
+const DELETE_AREAS_API_URL = `${process.env.REACT_APP_MY_DOMAIN}/delete_area`;
+//
+
+const GET_TASKS_API_URL = `${process.env.REACT_APP_MY_DOMAIN}/get_tasks`;
+const POST_TASKS_API_URL = `${process.env.REACT_APP_MY_DOMAIN}/post_tasks`;
+const DELETE_TASKS_API_URL = `${process.env.REACT_APP_MY_DOMAIN}/delete_task`;
+const UPDATE_COMPLETED_TASKS_URL = `${process.env.REACT_APP_MY_DOMAIN}/update_completed_task`;
+const UPDATE_TASK_API_URL = `${process.env.REACT_APP_MY_DOMAIN}/update_task`;
+
+export {
+  GET_AREAS_API_URL,
+  POST_AREAS_API_URL,
+  DELETE_AREAS_API_URL,
+  //
+  GET_TASKS_API_URL,
+  POST_TASKS_API_URL,
+  DELETE_TASKS_API_URL,
+  UPDATE_COMPLETED_TASKS_URL,
+  UPDATE_TASK_API_URL,
+};

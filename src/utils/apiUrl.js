dc612e77b67f5f6a09b4d87cfc9e04973c46d822
Jeongminseok-web <jeong010328@gmail.com>
2024-09-12 15:57:22 +0900

    plz

--- src/utils/apiUrl.js
+++ src/utils/apiUrl.js
@@ -1,13 +1,14 @@
-const GET_AREAS_API_URL = `${process.env.REACT_APP_MY_DOMAIN}/get_areas`;
-const POST_AREAS_API_URL = `${process.env.REACT_APP_MY_DOMAIN}/get_areas`;
-const DELETE_AREAS_API_URL = `${process.env.REACT_APP_MY_DOMAIN}/delete_area`;
+const GET_AREAS_API_URL = "https://campback.thxmin.com/get_areas";
+const POST_AREAS_API_URL = "https://campback.thxmin.com/get_areas";
+const DELETE_AREAS_API_URL = "https://campback.thxmin.com/delete_area";
 //
 
-const GET_TASKS_API_URL = `${process.env.REACT_APP_MY_DOMAIN}/get_tasks`;
-const POST_TASKS_API_URL = `${process.env.REACT_APP_MY_DOMAIN}/post_tasks`;
-const DELETE_TASKS_API_URL = `${process.env.REACT_APP_MY_DOMAIN}/delete_task`;
-const UPDATE_COMPLETED_TASKS_URL = `${process.env.REACT_APP_MY_DOMAIN}/update_completed_task`;
-const UPDATE_TASK_API_URL = `${process.env.REACT_APP_MY_DOMAIN}/update_task`;
+const GET_TASKS_API_URL = "https://campback.thxmin.com/get_tasks";
+const POST_TASKS_API_URL = "https://campback.thxmin.com/post_tasks";
+const DELETE_TASKS_API_URL = "https://campback.thxmin.com/delete_task";
+const UPDATE_COMPLETED_TASKS_URL =
+  "https://campback.thxmin.com/update_completed_task";
+const UPDATE_TASK_API_URL = "https://campback.thxmin.com/update_task";
 
 export {
   GET_AREAS_API_URL,
